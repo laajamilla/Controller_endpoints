d08721de3bf3bb97ce8ef5570777c76fb0fadaed
Milla Laaja <mmalinen78@gmail.com>
2022-08-25 10:14:22 +0300

    Initial commit

---
+++ src/main/java/sy22/contactMe/ContactMeApplication.java
@@ -0,0 +1,13 @@
+package sy22.contactMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sy22/contactMe/ContactMeApplicationTests.java
@@ -0,0 +1,13 @@
+package sy22.contactMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
